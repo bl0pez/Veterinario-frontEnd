bebff5ecb7df2bcd2d92928f97cd5cd57d6fcd54
b0pez <blopez4434@gmail.com>
2022-10-30 21:10:43 -0300

    limpiar authLayout

--- src/auth/layouts/AuthLayout.jsx
+++ src/auth/layouts/AuthLayout.jsx
@@ -1,16 +1,6 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { Spinner } from "../../ui/Spinner";
+import { Outlet } from 'react-router-dom';
 
 export const AuthLayout = () => {
-
-  if (false) {
-    return (
-      <div className="h-screen bg-gray-100 justify-center items-center flex">
-        <Spinner width="w-24" height="h-24" />
-      </div>
-    );
-  };
-
   return (
     <>
       <main className="container mx-auto md:grid md:grid-cols-2 gap-12 p-5 pt-20 items-center">
